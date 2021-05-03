5963f5853cca8ca94b390a177528ce0e1d9f0c5c
Daniella Marks <marks.daniella@gmail.com>
2021-05-03 16:21:32 -0700

    testing of one entry in creatures

--- __tests__/creatures.test.js
+++ __tests__/creatures.test.js
@@ -3,18 +3,36 @@ import supertest from 'supertest';
 import creatureData from '../data/creatures.js';
 const request = supertest(app);
 
-console.log(creatureData)
+console.log(creatureData);
 
 // If a GET reqest is made to /api/cats
 // does the server respond with status of 200
 // and body matching the cats data?
-it('GET /data/creatures', async () => {
+it('GET /data/creatures/', async () => {
 
-  const expected = creatureData
+  const expected = creatureData;
 
-  const response = await request.get('/data/creatures');
+  const response = await request.get('/data/creaturesData');
 
   expect(response.status).toBe(200); // 200 = OK
   expect(response.body).toEqual(expected);
 
+});
+
+it('GET /data/creaturesData/1', async () => {
+
+  const expected =  {
+    url: 
+        'http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg',
+    title: 'UniWhal',
+    description: 'A unicorn and a narwhal nuzzling their horns',
+    keyword: 'narwhal',
+    horns: 1
+  };
+  
+  const response = await request.get('/data/creaturesData/1');
+  
+  expect(response.status).toBe(200); // 200 = OK
+  expect(response.body).toEqual(expected);
+  
 });
